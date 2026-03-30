20ff705180b5e37b159cbcda0637372f1c7f910e
BANARIBA <bryananariba0@gmail.com>
2026-03-29 19:16:36 -0600

    Update content

--- first-steps/README.md
+++ first-steps/README.md
@@ -1,3 +1,3 @@
-# Ver Seccion 4 -> Introduccion a la seccion
+# Ver Seccion 4: modelo de respuesta parte 1
 
 # Como ver la documentacion de swagger que es por defecto en fastapi
--- first-steps/main.py
+++ first-steps/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, Query, Body, HTTPException
+from fastapi import FastAPI, Query, HTTPException
+from typing import List, Union
+
+from classes.post import PostPublic, PostCreate, PostUpdate, PostSumary
 
 BLOG_POSTS = [
   {"id": 1, "title": "Primer post", "content": "Contenido del 1 post"},
@@ -14,7 +17,11 @@ def home():
   return { 'message': 'Bienvenidos al mini blog' }
 
 # http://localhost:8000/posts?query=????
-@app.get("/posts")
+@app.get(
+  "/posts",
+  response_model=List[PostPublic],
+  response_description="Listado de comentarios"
+)
 def list_posts(query: str | None = Query(default=None, description="Texto para buscar por titulo del post")):
   if query:
     # List comprenhension
@@ -23,72 +30,68 @@ def list_posts(query: str | None = Query(default=None, description="Texto para b
     '''for post in BLOG_POSTS:
       if query.lower() in post["title"].lower():
         results.append(post)'''
-    return {
-      "data": results,
-      "query": query,
-    }
-  return {
-    "data": BLOG_POSTS
-  }
+    raise HTTPException(status_code=200, detail=results)
+  raise HTTPException(status_code=200, detail=BLOG_POSTS)
   
 # http://localhost:8000/posts/1
-@app.get("/posts/{post_id}")
-def get_post(post_id: int, query: bool | None = Query(default=True, description="Incluir el contenido del post True=Si, False= No")):
+@app.get(
+  "/posts/{post_id}",
+  response_model=Union[PostPublic, PostSumary],
+  response_description="Comentario encontrado"
+)
+def get_post(post_id: int, query: bool | None = Query(default=True, description="Incluir el contenido del post True=Si, False=No")):
   for post in BLOG_POSTS:
     if post["id"] == post_id:
       if query and query == True:
-        return {
-          "data": post
-        }
+        raise HTTPException(status_code=200, detail=post)
       else:
-        return {
-          "data": {
-            "id": post["id"],
-            "title": post["title"],
-          }
-        }
-  return {
+        raise HTTPException(status_code=200, detail={
+          "id": post["id"],
+          "title": post["title"],
+        })
+  raise HTTPException(status_code=404, detail={
     "error": "post no encontrado"
-  }
+  })
 
-@app.post("/posts")
-def create_post(post: dict = Body(...)):
-  if "title" not in post or "content" not in post:
-    return {
-      "error": "Titulo y contenido son requeridos"
-    }
-  if not str(post["title"]).strip():
-    return {
-      "error": "Titulo no puede estar vacio"
-    }
-  new_id:int = BLOG_POSTS[-1]["id"] + 1 if BLOG_POSTS else 1
+@app.post(
+  "/posts",
+  response_model=PostPublic,
+  response_description="Comentario creado"
+)
+def create_post(post: PostCreate):
+  new_post_id: int = BLOG_POSTS[-1]["id"] + 1 if BLOG_POSTS else 1
   new_post = {
-    "id": new_id,
-    "title": post["title"],
-    "content": post["content"]
+    "id": new_post_id,
+    "title": post.title,
+    "content": post.content,
+    "tags": [tag.model_dump() for tag in post.tags],
+    "author": post.author.model_dump() if post.author else None,
   }
   BLOG_POSTS.append(new_post)
-  return {
-    "message": "Comentario creado",
-    "data": new_post
-  }
+  raise HTTPException(status_code=201, detail=new_post)
   
-@app.put("/posts/{post_id}")
-def update_port(post_id: int, data: dict = Body(...)):
+@app.put(
+  "/posts/{post_id}",
+  response_model=PostPublic,
+  response_description="Comentario actualizado"
+)
+def update_post(post_id: int, data: PostUpdate):
   for post in BLOG_POSTS:
     if post["id"] == post_id:
-      if "title" in data: post["title"] = data["title"]
-      if "content" in data: post["content"] = data["content"]
-      return {
-        "message": "Comentario actualizado",
-        "data": post,
-      }
-    raise HTTPException(status_code=404, detail="Comentario no encontrado")
+      playload = data.model_dump(exclude_unset=True)
+      if "title" in playload: post["title"] = playload["title"]
+      if "content" in playload: post["content"] = playload["content"]
+      raise HTTPException(status_code=200, detail=post)
+    raise HTTPException(status_code=404, detail={ "error": "Comentario no encontrado" })
   
-@app.delete("/posts/{post_id}")
+@app.delete(
+  "/posts/{post_id}",
+  response_model=str,
+  response_description="Detalle del comentario ha eliminar"
+)
 def delete_post(post_id: int):
   for index, post in enumerate(BLOG_POSTS):
     if post["id"] == post_id:
       BLOG_POSTS.pop(index)
-      raise HTTPException(status_code=200, detail="Comentario eliminado")
-    raise HTTPException(status_code=404, detail="Comentario no encontrado")
+      raise HTTPException(status_code=200, detail=post)
+    raise HTTPException(status_code=404, detail={ "error": "Comentario no encontrado" })
